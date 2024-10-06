f1b95fcbd1cc7f793effcca5bade141f630f87cc
Arjun Sivakumar <arjunsivakumar2@gmail.com>
2024-10-06 17:20:17 +0530

    Lighting polished

--- Assets/Scripts/GenerateMaze.cs
+++ Assets/Scripts/GenerateMaze.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class GenerateMaze : MonoBehaviour
 {
-    [Header("Maze Settings")]
+    [Header("Generator Settings")]
     public int width;
     public int height;
+    public float deleteFrac;
 
     [Header("Tile Settings")]
     public GameObject wallTile;
     public float tilesize;
 
+    [Header("Level Settings")]
+    public GameObject floor;
+    public GameObject ceiling;
+
+
     enum dir{n,e,w,s,o};
+    GameObject[,] tiles;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //create the floor
+        Transform f = Instantiate(floor, new Vector3(width * tilesize, -.5f, height * tilesize), Quaternion.identity).transform;
+        f.localScale = new Vector3(2*width + 1, 1/tilesize, 2*height + 1) * tilesize;
+        
+        //create the ceiling
+        Transform c = Instantiate(ceiling, new Vector3(width * tilesize, 3.5f, height * tilesize), Quaternion.identity).transform;
+        c.localScale = new Vector3(2*width + 1, 1/tilesize, 2*height + 1) * tilesize;
 
         // create the paths
         dir[,] maze = new dir[width, height];
@@ -31,7 +47,7 @@ public class GenerateMaze : MonoBehaviour
         maze[width-1, height-1] = dir.o;
         Vector2Int origin = new Vector2Int(width-1, height-1);
 
-        for(int i = 0; i < width * height * 10; i++){
+        for(int i = 0; i < width * height * 25; i++){
             dir originMoveDir = (dir)Random.Range(0,4);
             if(origin.x == 0 && originMoveDir == dir.n)
                 originMoveDir = dir.s;
@@ -53,11 +69,12 @@ public class GenerateMaze : MonoBehaviour
 
 
         // create the tiles for the scene
-        GameObject[,] tiles = new GameObject[2*width+1, 2*height+1];
+        tiles = new GameObject[2*width+1, 2*height+1];
 
         for(int i = 0; i < 2*width + 1; i++){
             for(int j = 0; j < 2*height + 1; j++){
                 tiles[i, j] = Instantiate(wallTile, new Vector3(i, 0, j) * tilesize, Quaternion.identity);
+                tiles[i, j].name = "wall"+i+","+j;
             }
         }
 
@@ -78,13 +95,29 @@ public class GenerateMaze : MonoBehaviour
         // randomly remove 20% of walls to form loops
         for(int i = 1; i < 2*width; i++){
             for(int j = 1; j < 2*height; j++){
-                if(tiles[i,j].activeSelf){
-                    if((!tiles[i-1,j].activeSelf && !tiles[i+1,j].activeSelf) || (!tiles[i,j-1].activeSelf && !tiles[i,j+1].activeSelf)){
-                            if(Random.Range(0f, 1f) <= 0.1)
-                                tiles[i,j].SetActive(false);
-                        }
+                if(tiles[i,j].activeSelf && isRemovable(i, j)){
+                    if(Random.Range(0f, 1f) <= deleteFrac)
+                        tiles[i,j].SetActive(false);
                 }
             }
-        } 
+        }
+
+        //cut out entrance and exit
+        //tiles[0,1].SetActive(false);
+        tiles[2*width, 2*height-1].SetActive(false);
+    }
+
+    private bool isRemovable(int i,int j){
+        int code = 0;
+        if(tiles[i-1,j].activeSelf)
+            code += 1000;
+        if(tiles[i+1,j].activeSelf)
+            code += 100;
+        if(tiles[i,j-1].activeSelf)
+            code += 10;
+        if(tiles[i,j+1].activeSelf)
+            code += 1;
+
+        return code == 1100 || code == 0011;
     }
 }
--- Assets/Scripts/PlayerTorch.cs
+++ Assets/Scripts/PlayerTorch.cs
@@ -24,7 +24,7 @@ public class PlayerTorch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)){
+        if(Input.GetMouseButtonDown(0)){
             on = !on;
         }
 
