bce82064551aeae891f677f1a15c0ef71e265b53
Arjun Sivakumar <arjunsivakumar2@gmail.com>
2024-10-06 12:01:29 +0530

    Player camera finished

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform player;
+    public Transform camPos;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.position = camPos.position;
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        player.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
